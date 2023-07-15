a6a4fe2453e152ae8127c3209bc97ea4d92da541
kmrabhii <51476717+kmrabhii@users.noreply.github.com>
2023-07-15 18:04:39 +0530

    Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -3,28 +3,31 @@ import random
 
 print("Number Guessing Game".center(50))
 
-
-x=int(input("Enter First Number:- "))
-y=int(input("Enter Second Number:- "))
+x=int(input("Enter Lower Number:- "))
+y=int(input("Enter Upper Number:- "))
 z=int(random.randint(x,y))
 
-c=12
+if y>=50:
+    c = y // 10
+else:
+    c=5
 n=0
+print("Search Between",x,"and",y)
 print("You Have Only",c,"Chances.")
 
 while c>0:
     n=int(input("Enter Number:- "))
     if n==z:
-        print("Congrats You Win..")
+        print("Congrats You Win..\nThe Number is:- ",z)
         break
     elif n>z:
-        print("Your Number is Greater..")
+        print("Your Number is Greater..\nTry Smaller Number.")
         c=c-1
-        print("You Have ",c,"Chances Left")
+        print("You Have",c,"Chances Left.")
     elif n<z:
-        print("Your Number is Smaller..")
+        print("Your Number is Smaller..\nTry Bigger Number.")
         c=c-1
-        print("You Have ",c,"Chances Left")
+        print("You Have",c,"Chances Left.")
 
 if c==0:
-    print("You Loose..\nBetter Luck Next Time.")
+    print("You Loose..\nNumber is:-",z,"\nBetter Luck Next Time.")
