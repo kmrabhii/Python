57bd4f626f670547c702f90f455952440410eee9
kmrabhii <51476717+kmrabhii@users.noreply.github.com>
2023-07-15 17:44:04 +0530

    Add files via upload

---
+++ Number Guessing Game.py
@@ -0,0 +1,30 @@
+#  Number Guessing Game
+import random
+
+print("Number Guessing Game".center(50))
+
+
+x=int(input("Enter First Number:- "))
+y=int(input("Enter Second Number:- "))
+z=int(random.randint(x,y))
+
+c=3
+n=0
+print("You Have Only",c,"Chances.")
+
+while c>0:
+    n=int(input("Enter Number:- "))
+    if n==z:
+        print("Congrats You Win..")
+        break
+    elif n>z:
+        print("Your Number is Greater..")
+        c=c-1
+        print("You Have ",c,"Chances Left")
+    elif n<z:
+        print("Your Number is Smaller..")
+        c=c-1
+        print("You Have ",c,"Chances Left")
+
+if c==0:
+    print("You Loose..\nBetter Luck Next Time.")
